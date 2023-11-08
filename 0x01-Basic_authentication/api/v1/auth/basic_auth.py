63fca15fcea05f80cdefe3f623dec41aaf9ee48d
Mwaura Mbugua <mwaurathealex@gmail.com>
2023-11-08 09:49:40 +0300

    8. Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """ Basic Auth Module"""
+import base64
 from api.v1.auth.auth import Auth
 
 
@@ -19,3 +20,18 @@ class BasicAuth(Auth):
             return None
 
         return (header_values[1] if len(header_values) == 2 else None)
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """ Decodes base64 authoriztion header"""
+        if base64_authorization_header is None:
+            return None
+        if type(base64_authorization_header) is not str:
+            return None
+
+        try:
+            base64_bytes = base64_authorization_header.encode('utf-8')
+            message_bytes = base64.b64decode(base64_bytes)
+            return message_bytes.decode('utf-8')
+        except Exception:
+            return None
