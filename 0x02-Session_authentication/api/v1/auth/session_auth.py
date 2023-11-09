eb2c001f53d9985317f2c55828614dd639f80642
Mwaura Mbugua <mwaurathealex@gmail.com>
2023-11-09 10:52:46 +0300

    5. Before request

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -12,7 +12,7 @@ class SessionAuth(Auth):
     def create_session(self, user_id: str = None) -> str:
         """ Creates a Session ID for a user_id"""
         if type(user_id) is str:
-            session_id = uuid.uuid4()
+            session_id = str(uuid.uuid4())
             self.user_id_by_session_id[session_id] = user_id
 
             return session_id
