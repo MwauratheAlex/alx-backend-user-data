483b789770f1059335903f962e87e7f9e24308c8
Mwaura Mbugua <mwaurathealex@gmail.com>
2023-11-09 08:44:07 +0300

    5. Before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -49,30 +49,21 @@ def forbidden(error) -> str:
 @app.before_request
 def before_request():
     """filters each request"""
-    if auth is None:
-        return
-    
-    user = auth.current_user(request)
-    excluded_paths = [
-            '/api/v1/status/',
-            '/api/v1/unauthorized/',
-            '/api/v1/forbidden/',
-            '/api/v1/auth_session/login/']
-    if not auth.require_auth(request.path, excluded_paths):
-        return
-
-    if (auth.authorization_header(request) is None and
-            auth.session_cookie(request) is None):
-        abort(401)
-
-    if auth.authorization_header(request) is None:
-        abort(401)
-
-    if user is None:
-        abort(403)
-
-    request.current_user = user
-
+    if auth:
+        excluded_paths = [
+            "/api/v1/status/",
+            "/api/v1/unauthorized/",
+            "/api/v1/forbidden/",
+            "/api/v1/auth_session/login/",
+        ]
+        if auth.require_auth(request.path, excluded_paths):
+            user = auth.current_user(request)
+            if auth.authorization_header(request) is None and \
+                    auth.session_cookie(request) is None:
+                abort(401)
+            if user is None:
+                abort(403)
+            request.current_user = user
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
