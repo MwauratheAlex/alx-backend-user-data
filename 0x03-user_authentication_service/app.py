60034ebe8d3b5b7aedfeb6528efab8f1e65639cb
Mbugua <mwaurathealex@gmail.com>
2024-02-19 16:23:54 +0300

    update password route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -82,5 +82,20 @@ def get_reset_password_token():
         abort(403)
 
 
+@app.route("/reset_password", methods=["PUT"])
+def update_password():
+    """PUT /reset_password"""
+    email = request.form.get("email")
+    reset_token = request.form.get("reset_token")
+    new_password = request.form.get("new_password")
+    if not (email and reset_token and new_password):
+        abort(403)
+    try:
+        AUTH.update_password(reset_token, new_password)
+    except Exception:
+        abort(403)
+    return jsonify({"email": email, "message": "Password updated"})
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
