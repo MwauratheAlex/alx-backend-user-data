5d3d6e9115978de472607fe8d200fe5dc440564d
Mbugua <mwaurathealex@gmail.com>
2024-02-19 13:04:33 +0300

    save user session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -44,3 +44,13 @@ class Auth:
             )
         except Exception:
             return False
+
+    def create_session(self, email: str):
+        """Creates a session for user"""
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except Exception:
+            return None
