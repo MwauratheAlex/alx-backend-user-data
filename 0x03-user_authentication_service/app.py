738041fd551fda38f1249a2cbc14f01d932fe925
Mbugua <mwaurathealex@gmail.com>
2024-02-19 12:01:48 +0300

    register user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,15 +1,31 @@
 #!/usr/bin/env python3
 """Module contais a flask app"""
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+AUTH = Auth()
 
 
 @app.route("/", methods=["GET"])
 def index():
-    """Index page"""
-    return jsonify({"message": "Bienvenue"})
+    """GET /"""
+    return jsonify({"message": "Mbumwa auth service"})
+
+
+@app.route("/users", methods=["POST"])
+def users():
+    """POST /users"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if not email or not password:
+        return jsonify({'error': 'email and password are required'}), 400
+    try:
+        AUTH.register_user(email, password)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+    return jsonify({"email": email, "message": "user created"})
 
 
 if __name__ == "__main__":
