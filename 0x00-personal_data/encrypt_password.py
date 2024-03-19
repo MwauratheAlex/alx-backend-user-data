42532d27178003aba9f761c30f5ed32867bf3a21
Mbugua <mwaurathealex@gmail.com>
2024-03-19 12:45:50 +0300

    check valid password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -9,3 +9,9 @@ def hash_password(password: str) -> bytes:
     pwd_bytes = password.encode('utf-8')
     salt = bcrypt.gensalt()
     return bcrypt.hashpw(pwd_bytes, salt)
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Validates a password"""
+    pwd_bytes = password.encode('utf-8')
+    return bcrypt.checkpw(pwd_bytes, hashed_password)
