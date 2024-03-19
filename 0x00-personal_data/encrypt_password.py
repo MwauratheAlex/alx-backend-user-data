0288d9fe08c19462a231bb440c7c1234f256f8ea
Mbugua <mwaurathealex@gmail.com>
2024-03-19 12:41:02 +0300

    encrypting passwords

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """Hashes a password and returns hashed password"""
+    pwd_bytes = password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    return bcrypt.hashpw(pwd_bytes, salt)
